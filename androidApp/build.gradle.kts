19bcdc7198ec32819cee6c9b952170f386e43b57
Ryan <ryanzzff@gmail.com>
2025-04-13 20:03:53 +0800

    feat: Increment version code to 2 in build configuration

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.ryzoft.bondportfolioapp.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
